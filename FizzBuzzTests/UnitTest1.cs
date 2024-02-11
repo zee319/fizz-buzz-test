94ca0698e6b314af58a7c4729fc29a5138d425a6
zeesibanda <111881209+zeesibanda@users.noreply.github.com>
2024-02-11 22:36:59 +0000

    Initial unit tests - failing

---
+++ FizzBuzzTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzServiceTests
+    {
+      
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenDivisibleByThreeShouldReturnFizz()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Equals("Fizz", null);
+        }
+
+        [Test]
+        public void WhenDivisibleByFiveShouldReturnBuzz()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.Equals("Buzz", null);
+        }
+
+        [Test]
+        public void WhenDivisibleByFiveAndThreeShouldReturnFizzBuzz()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Equals("FizzBuzz", null);
+        }
+    }
+}
