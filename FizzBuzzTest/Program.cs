f1da7eb7790e91d15268e4e9876badce63743876
zeesibanda <111881209+zeesibanda@users.noreply.github.com>
2024-02-11 22:51:09 +0000

    Initial printing of FizzBuzz logic

--- FizzBuzzTest/Program.cs
+++ FizzBuzzTest/Program.cs
@@ -10,6 +10,34 @@ namespace FizzBuzzTest
     {
         static void Main(string[] args)
         {
+            //Printing FizzBuzz Result
+            Console.WriteLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string result = GetFizzBuzz(i);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+
+        static string GetFizzBuzz(int n)
+        {
+            string result = n.ToString(); 
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                result =  "FizzBuzz";
+            }
+            if (n % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            if (n % 5 == 0)
+            {
+                result =  "Buzz";
+            }
+            return result;    
         }
     }
 }
